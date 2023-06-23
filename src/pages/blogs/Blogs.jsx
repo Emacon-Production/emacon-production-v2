3d597ad376e33daa69aab4a366070b8d4eeca1d3
GithuiVictor <victorgithuim@gmail.com>
2023-06-23 10:46:11 +0300

    Fix: Flex-end

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './blogs.css'
 import Navigation from '../../components/navigation/Navigation'
 import Footer from '../../components/footer/Footer'
 
@@ -9,7 +10,10 @@ const Blogs = () => {
     <div>
         <Navigation />
 
-        
+        <div className='blogsage_sectionone'>
+            <img src='https://res.cloudinary.com/emacon-production/image/upload/v1687443940/Emacon%20Production/undraw_under_construction_46pa_1_hy4whe.png' alt='Emacon Production | Blog' />
+            <h1>COMMING SOON...</h1>
+        </div>
 
         <Footer />
     </div>
